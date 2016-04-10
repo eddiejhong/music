8a449f1498105f3b328e76cdcef0914fa9023e76
Eddie Hong <eddiejhong@gmail.com>
2016-04-09 18:08:51 -0700

    reflection placeholder bugfix, update .gitignore

--- app/reflection.component.ts
+++ app/reflection.component.ts
@@ -2,7 +2,7 @@ import { Component } from 'angular2/core';
 import { Router } from 'angular2/router';
 
 @Component({
-    templateUrl: '<div>reflect</div>'
+    template: '<div>reflect</div>'
 })
 export class ReflectionComponent {
     constructor(
